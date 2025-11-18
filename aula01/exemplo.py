d08acd647f88966a53c590977f934101c50eb2bc
leo2lc <leonardolucas-c@hotmail.com>
2025-11-18 19:41:34 -0300

    Inicia o repositório da aula 01

---
+++ aula01/exemplo.py
@@ -0,0 +1,74 @@
+# nome = 'Ana'
+# idade = 25
+# salario = 15000.0
+
+# print(f'O nome ela é {nome}, \n idade {idade} \n salário {salario}')
+
+# x = 5
+# x = '10'
+# print(x)
+
+# nome = input('Digite o nome: ')
+# print(f'Hello, {nome}')
+
+# cor = input('Qual sua cor favorita?')
+# print(f'Hello, {cor}')
+
+# n1 = 5
+# n2 = 3
+
+# resultado = n1 + n2
+# print(f'O resultad é: {resultado}')
+
+# n1 = float(input('Digite um valor: '))
+# n2 = float(input('Digite outro valor para ser somado: '))
+# resultado = n1+n2
+# print(f'O resultado é {resultado}')
+
+#______________________________________________________________________________________
+
+# nome = input('Digite o seu nome: ')
+# idade = int(input('Digite sua idade: '))
+# cidade = input('Qual cidade você mora: ')
+# bairro = input('Em que bairro: ')
+
+# print(f'Olá {nome}, você tem {idade} anos, você mora na cidade de(o/a) {cidade} no bairro {bairro}')
+
+# if  idade < 18: 
+#     print('você não pode beber')
+# else:
+#     print('você pode beber')
+#______________________________________________________________________________________
+
+# idade = int(input('Quantos anos você tem? '))
+# ano = int(2025)
+# print(f'Você nasceu em {ano-idade} ')
+
+# if ano-idade > 20:
+#     print('tá velho')
+# else:
+#     print('Jovem ainda')
+#______________________________________________________________________________________
+
+# valoringresso = float(input('Digite o valor do ingresso: R$ '))
+# money = float(input('Quanto de grana você tem? R$ '))
+# resultado = money / valoringresso
+# troco = money % valoringresso
+# print(f'Você pode comprar {resultado:.2f} ingressos e sobra R$ {troco:.2f} de troco')
+______________________________________________________________________________________
+
+# preco = float(input(f'Informe o preço: '))
+# qntd = int(input('Informe a quantidade: '))
+# total = preco * qntd
+# desconto = total * 0.1
+# vlrfinal = total - desconto
+# print(f'Valor total {total} \n desconto de 10% {desconto} \n valor a pagar {vlrfinal}')
+
+#______________________________________________________________________________________
+#O algorítmo pergunta o salário e acrescenta um aumento percentual, e no final mostra o salário antigo e o salário com acréscimo
+
+# salario = float(input('Qual o seu salário? R$'))
+# aumento = float(input('Quanto deseja de aumento percetual de salário?'))
+# slraumentado = salario * (1 + aumento)
+# print(f'Seu salário era de {salario}, com o acréscimo ficou em {slraumentado}')
+
